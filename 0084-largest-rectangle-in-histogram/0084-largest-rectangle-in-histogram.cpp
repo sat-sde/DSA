970b779e814550e1722285cf1fc4930d7c6a24d7
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-06-29 12:29:26 +0530

    Time: 6 ms (98.49%), Space: 81.4 MB (72.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,60 +1,27 @@
 class Solution {
-    private:
-    vector<int> nextSmallerElement(vector<int> arr, int n) {
-        stack<int> s;
-        s.push(-1);
-        vector<int> ans(n);
-
-        for(int i=n-1; i>=0 ; i--) {
-            int curr = arr[i];
-            while(s.top() != -1 && arr[s.top()] >= curr)
-            {
-                s.pop();
-            }
-            //ans is stack ka top
-            ans[i] = s.top();
-            s.push(i);
-        }
-        return ans;
-    }
-    
-    vector<int> prevSmallerElement(vector<int> arr, int n) {
-        stack<int> s;
-        s.push(-1);
-        vector<int> ans(n);
-
-        for(int i=0; i<n; i++) {
-            int curr = arr[i];
-            while(s.top() != -1 && arr[s.top()] >= curr)
-            {
-                s.pop();
-            }
-            //ans is stack ka top
-            ans[i] = s.top();
-            s.push(i);
-        }
-        return ans; 
-    }
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int>prev(n);
-        prev = prevSmallerElement(heights,n);
-
-        vector<int>next(n);
-        next = nextSmallerElement(heights,n);
-
-        int area =INT_MIN;
+        int n=heights.size();
+        stack<int>st;
+        int maxi=INT_MIN;
         for(int i=0;i<n;i++){
-            int l = heights[i];
-            if(next[i]==-1){
-                next[i]=n;
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int ind=st.top();
+                st.pop();
+                int nse=i;
+                int pse= (st.empty()) ? -1 : st.top();
+                maxi=max(maxi,heights[ind]*(nse-pse-1));
             }
-            int b = next[i]-prev[i]-1;
-            int newarea=l*b;
-            area=max(area,newarea);
-
+            st.push(i);
         }
-        return area;
+
+        while(!st.empty()){
+            int nse=n;
+            int ind=st.top();
+            st.pop();
+            int pse= (st.empty()) ? -1 : st.top();
+             maxi=max(maxi,heights[ind]*(nse-pse-1));
+        }
+        return maxi;
     }
 };
