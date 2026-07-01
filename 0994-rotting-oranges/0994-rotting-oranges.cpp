c93b73c7da77ca06ce7dc3eafabd58d8d2e6817f
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-07-01 19:39:20 +0530

    Time: 7 ms (5.6%), Space: 16.8 MB (52.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,7 +4,6 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
