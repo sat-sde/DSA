a1293ac86b9582ed52e4daf8ef9e11d8793ff7fb
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-07-07 12:10:51 +0530

    Time: 39 ms (36.99%), Space: 18.2 MB (93.5%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,27 +1,53 @@
-class Solution {
-public:
-    void dfs(vector<bool>&vis, vector<vector<int>>& stones, int ind) {
-        int n = stones.size();
-        vis[ind] = true;
-        for (int i = 0; i < n; i++) {
-            int r = stones[ind][0];
-            int c = stones[ind][1];
-            if (vis[i] == false && (stones[i][0] == r || stones[i][1] == c)) {
-                dfs(vis, stones, i);
-            }
+class DisjointSet{
+    vector<int>rank,parent;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
     }
-    int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size();
-        int group = 0;
-        vector<bool> vis(n, false);
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == true) {
-                continue;
-            }
-            dfs(vis,stones,i);
-            group++;
+
+    int findUpar(int node){
+        if(node==parent[node]){
+            return node;
         }
-        return n - group;
+        return parent[node]=findUpar(parent[node]);
+    }
+
+    void UnionbyRank(int u,int v){
+        int ul_p=findUpar(u);
+        int vl_p=findUpar(v);
+        if(ul_p==vl_p) return;
+        if(rank[ul_p]<rank[vl_p]){
+            parent[ul_p]=vl_p;
+        }else if(rank[vl_p]<rank[ul_p]){
+             parent[vl_p]=ul_p;
+        }else{
+             parent[ul_p]=vl_p;
+             rank[vl_p]++;
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    ds.UnionbyRank(i,j);
+                }
+            }
+        }
+        int grp=0;
+        for(int i=0;i<n;i++){
+            if(ds.findUpar(i)==i){
+                grp++;
+            }
+        }
+        return n-grp;
     }
 };
