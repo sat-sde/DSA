d5f567c74b2b5ff5ca99c2a697cb2ec61a24fd43
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-07-10 10:29:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.68%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[101][101];
+    int uniquePaths(int m, int n) {
+        memset(dp,0,sizeof(dp));
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) continue;
+                int down=0;
+                int right=0;
+                if(i+1<m){
+                    down=dp[i+1][j];
+                }
+                 if(j+1<n){
+                    right=dp[i][j+1];
+                }
+                dp[i][j]=down+right;
+            }
+        }
+        return dp[0][0];
+    }
+};
