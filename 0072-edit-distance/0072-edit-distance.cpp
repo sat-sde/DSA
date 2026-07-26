725ee26359b4e4d91de528666d4e5d0e39f66000
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-07-26 15:54:04 +0530

    Time: 40 ms (13.01%), Space: 10.6 MB (90%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int dp[501][501];
-    int solve(int i,int j,string &s,string &t){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            dp[i][j]=solve(i-1,j-1,s,t);
-        }else{
-            int insert=1+solve(i,j-1,s,t);
-            int del=1+solve(i-1,j,s,t);
-            int repl=1+solve(i-1,j-1,s,t);
-            dp[i][j]=min({insert,del,repl});
-        }
-        return dp[i][j];
-    }
     int minDistance(string word1, string word2) {
-        int m=word1.size();
-        int n=word2.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(m-1,n-1,word1,word2);
+        int m = word1.size();
+        int n = word2.size();
+        memset(dp, 0, sizeof(dp));
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    int insert = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int repl = 1 + dp[i-1][j-1];
+                    dp[i][j] = min({insert, del, repl});
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
