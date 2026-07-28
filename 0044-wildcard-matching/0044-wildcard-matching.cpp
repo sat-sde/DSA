f6f077ba53e3be0a90e04052b2bf5ac6f16cdbd0
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-07-28 19:12:16 +0530

    Time: 1304 ms (5.01%), Space: 26.9 MB (46.94%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[2001][2001];
+    bool solve(int i,int j,string &s,string &p){
+        if(i<0 && j<0) return true;
+        if(i<0 && j>0) return false;
+        if(j<0){
+            for(int k=0;k<=i;k++){
+                if(s[k]!='*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==p[j] || s[i]=='?'){
+            return dp[i][j]=solve(i-1,j-1,s,p);
+        }else if(s[i]=='*'){
+            return dp[i][j]=solve(i-1,j,s,p) || solve(i,j-1,s,p);
+        }
+        return dp[i][j]=false;
+    }
+    bool isMatch(string s, string p) {
+        int m=s.length();
+        int n=p.length();
+        memset(dp,-1,sizeof(dp));
+        return solve(n-1, m-1, p, s);
+    }
+};
