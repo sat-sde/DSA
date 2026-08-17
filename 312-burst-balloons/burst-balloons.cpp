9be6e4fc863194d85f0c4529ff000a4b8df88967
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-08-17 14:53:28 +0530

    Time: 155 ms (55.19%) | Memory: 10.9 MB (98.70%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[301][301];
+    int solve(int i,int j,vector<int>&nums){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int maxi=INT_MIN;
+        for(int ind=i;ind<=j;ind++){
+            int steps = nums[i-1] * nums[ind] * nums[j+1] + solve(i,ind-1,nums)+solve(ind+1,j,nums);
+            maxi=max(maxi,steps);
+        }
+        return dp[i][j]=maxi;
+    }
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        memset(dp,-1,sizeof(dp));
+        return solve(1,n,nums);
+    }
+};
